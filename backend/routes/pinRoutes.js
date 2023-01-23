611308034f88bb572001533bb3df53fefc65b729
Revati <revati.v.bordawekar@gmail.com>
2023-01-23 10:21:58 -0800

    pinRoutes working

--- backend/routes/pinRoutes.js
+++ backend/routes/pinRoutes.js
@@ -17,5 +17,13 @@ router.post("/", async(req,res) => {
 
 //Get all Pins
 
+router.get("/", async(req,res) => {
+    try {
+        const pins = await Pin.find();
+        res.status(200).json(pins);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 module.exports = router;
