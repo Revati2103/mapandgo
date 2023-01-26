c774dd83f5aef96600c5d87e48783bb723f7da6a
Revati <revati.v.bordawekar@gmail.com>
2023-01-25 18:27:43 -0800

    working on userRoutes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,37 @@
+const router = require("express").Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+//Register
+
+router.post("/register", async(req,res) => {
+    const { username, email, password } = req.body
+    try {
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+    })
+      
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+//Login
+
+router.get("/", async(req,res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+module.exports = router;
