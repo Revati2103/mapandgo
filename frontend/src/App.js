59c2323b2a598ff8dd38e8091e57d033eb0ff0bc
Revati <revati.v.bordawekar@gmail.com>
2023-01-26 14:51:11 -0800

    setting up mapbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
-
+import { useState } from "react";
+import Map, {Marker} from 'react-map-gl'
 
 function App() {
+
+  // const [viewport, setViewport] = useState({
+  //   width: "100vw",
+  //   height: "100vh",
+  //   latitude: 47.040182,
+  //   longitude: 17.071727,
+  //   zoom: 4,
+  // });
+
   return (
     <div className="App">
-      hello
+      <Map
+    initialViewState={{
+      longitude: -122.4,
+      latitude: 37.8,
+      zoom: 14
+    }}
+    style={{width: "100vw", height: "100vh"}}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+    mapboxAccessToken= {process.env.REACT_APP_MAPBOX} >
+     
+     <Marker longitude={-100} latitude={40} anchor="bottom" >
+      
+    </Marker>
+ 
+    </Map>
+
     </div>
   );
 }
