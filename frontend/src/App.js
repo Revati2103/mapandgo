43b35a034a953b9eaeb78df07774c303a93bc8e9
Revati <revati.v.bordawekar@gmail.com>
2023-01-27 11:42:20 -0800

    fixed popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,17 +4,17 @@ import { Room, Star} from '@mui/icons-material';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './app.css'
 import axios from 'axios'
+import { format } from "timeago.js";
 
 function App() {
-  const [pins, setPins] = useState([])
+  const [pins, setPins] = useState([]);
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+
   const [viewport, setViewport] = useState({
-    width:"100vw",
-    height: "100vh",
     latitude: 47.040182,
     longitude: 17.071727,
     zoom: 4,
   });
-
   useEffect(() => {
     const getPins = async () => {
       try {
@@ -27,16 +27,22 @@ function App() {
     getPins();
   }, []);
 
+  const handleMarkerClick = (id, lat, long) => {
+    setCurrentPlaceId(id);
+    setViewport({ ...viewport, latitude: lat, longitude: long });
+  };
+
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <ReactMapGL
       {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
+      onViewportChange={(viewport) => setViewport(viewport)}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+      width="100%"
+      height="100%"
       mapStyle="mapbox://styles/safak/cknndpyfq268f17p53nmpwira"
       >
-     
-     {pins.map((p) => (
+    {pins.map((p) => (
           <>
             <Marker
               latitude={p.lat}
@@ -44,38 +50,48 @@ function App() {
               offsetLeft={-3.5 * viewport.zoom}
               offsetTop={-7 * viewport.zoom}
             >
-     <Room style={{
-      fontSize: viewport.zoom *7,
-      color: "slateblue"
-     }}/>
-    </Marker>
-     <Popup 
-         longitude={p.long} 
-        latitude={p.lat}
-        closeButton={true}
-        closeOnClick={false}
-        anchor="left"
-        >
-        <div className='card'>
-          <label>Place</label>
-          <h4 className='place'>{p.title}</h4>
-          <label>Review</label>
-          <p className='desc'>{p.description}</p>
-          <label>Rating</label>
-          <div className='stars'>
+              <Room
+                style={{
+                  fontSize: 7 * viewport.zoom,
+                  color:"slateblue",
+                  cursor: "pointer",
+                }}
+                onClick={() => handleMarkerClick(p._id, p.lat, p.long)}
+              />
+            </Marker>
+            {p._id === currentPlaceId && (
+              <Popup
+                key={p._id}
+                latitude={p.lat}
+                longitude={p.long}
+                closeButton={true}
+                closeOnClick={false}
+                onClose={() => setCurrentPlaceId(null)}
+                anchor="left"
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="desc">{p.desc}</p>
+                  <label>Rating</label>
+                  <div className='stars'>
             <Star className='stars'/>
             <Star className='stars'/>
             <Star className='stars'/>
             <Star className='stars'/>
             <Star className='stars'/>
          </div>
-        <label>Information</label>
-         <span className='username'>Created by <b> {p.username} </b></span>
-          <span className='date'>1 hour ago</span>
-        </div>
-      </Popup>
-    </>
-     ))}
+                  <label>Information</label>
+                  <span className="username">
+                    Created by <b>{p.username}</b>
+                  </span>
+                  <span className="date">{format(p.createdAt)}</span>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
      
     </ReactMapGL>
 
